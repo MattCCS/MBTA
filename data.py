9ef79f03c12b232f182aed171aeb3ed163043e29
Matthew Cotton <matt@thecottons.com>
2016-09-13 00:11:32 -0400

    Fix conversion

--- data.py
+++ data.py
@@ -5,7 +5,7 @@ import datetime
 class DataPoint(object):
 
     def __init__(self, timestamp, predicted_delta_seconds):
-        self.timestamp = timestamp
+        self.timestamp = datetime.datetime.fromtimestamp(timestamp / 1e3)
         self.predicted_delta_seconds = [datetime.timedelta(seconds=t) for t in sorted(predicted_delta_seconds)]
 
     def datetime(self):
@@ -18,4 +18,4 @@ class DataPoint(object):
         return self.predicted_delta_seconds[0]
 
     def next_predicted(self):
-        return self.timestamp.time() + self.next_delta()
+        return (self.timestamp + self.next_delta()).time()
